67023002c94ad2fd97cec4a6d1ca71e4c820bb75
marcos gomes <marcos.gomes@modenaesilva.com.br>
2024-09-02 13:22:33 -0400

    Atualizando endpoints para usar o banco de dados real

--- fast_zero/main.py
+++ fast_zero/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
 from http import HTTPStatus
-from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB, UserList
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fast_zero.database import get_session
+from fast_zero.models import User
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 database = []
@@ -47,37 +55,70 @@ def read_html():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
-    
-    database.append(user_with_id)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
 
-    return user_with_id
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists'
+            )
+        elif db_user.email == user.email:
+             raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
+        
+     
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+    return {'users': users}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1: 
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        ) 
-
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id 
-
-    return user_with_id
-
-@app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    del database[user_id - 1]
 
-    return {'message': 'User deleted' }
+    db_user.username = user.username
+    db_user.password = user.password
+    db_user.email = user.email
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    session.delete(db_user)
+    session.commit()
+
+    return {'message': 'User deleted'}
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,21 +1,21 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
+
 
 class Message(BaseModel):
     message: str
-
 
 class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
 
+
 class UserPublic(BaseModel):
     id: int
     username: str
     email: EmailStr
+    model_config = ConfigDict(from_attributes=True)
 
-class UserDB(UserSchema):
-    id: int
 
 class UserList(BaseModel):
     users: list[UserPublic]
