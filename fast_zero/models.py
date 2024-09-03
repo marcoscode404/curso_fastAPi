ecfc4d438ec08466356f0fe895d87e6060c3abbf
marcos gomes <marcos.gomes@modenaesilva.com.br>
2024-09-03 09:45:27 -0400

    Implementado os endpoints de tarefas

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,10 +1,18 @@
 from datetime import datetime
+from enum import Enum
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 table_registry = registry()
 
+
+class TodoState(str, Enum):
+    draft = 'draft'
+    todo = 'todo'
+    doing = 'doing'
+    done = 'done'
+    trash = 'trash'
 
 @table_registry.mapped_as_dataclass
 class User:
@@ -17,3 +25,20 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+
+    todos: Mapped[list['Todo']] = relationship(
+        init=False, back_populates='user', cascade='all, delete-orphan'
+    )
+
+@table_registry.mapped_as_dataclass
+class Todo:
+    __tablename__ = 'todos'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    state: Mapped[TodoState]
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped[User] = relationship(init=False, back_populates='todos')
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
-
+from fast_zero.models import TodoState
 
 class Message(BaseModel):
     message: str
@@ -29,3 +29,23 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+
+
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+    state: TodoState
+
+
+class TodoPublic(TodoSchema):
+    id: int
+
+
+class TodoList(BaseModel):
+    todos: list[TodoPublic]
+
+
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    state: TodoState | None = None
